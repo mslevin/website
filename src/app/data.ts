b0255d3c2517a5336c1b631b9b2b124f9694dd4a
Michael Slevin <michael@complete.so>
2025-08-18 21:58:43 -0700

    adding some content

---
+++ src/app/data.ts
@@ -0,0 +1,23 @@
+
+
+export const urls: {
+  name: string;
+  url: string;
+}[] = [
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/michaelslevin11",
+  },
+  {
+    name: "GitHub",
+    url: "https://github.com/mslevin",
+  },
+  {
+    name: "Instagram",
+    url: "https://www.instagram.com/michael_slevin",
+  },
+  {
+    name: "imnotspeeding.com",
+    url: "https://www.imnotspeeding.com",
+  },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-"use client";
-
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import Link from "next/link";
+import { urls } from "./data";
 
 export default function Home() {
   const bytes = [
@@ -10,56 +9,77 @@ export default function Home() {
   ];
   const address = String.fromCharCode(...bytes);
   return (
-    <main>
-      <div className="flex font-[Times_New_Roman] p-8 gap-8">
-        <div className="flex gap-4">
-          <Image
-            className="dark:invert"
-            src="/img/flowers_crop.jpeg"
-            alt="Me, standing in front of a flower bush."
-            width={300}
-            height={38}
-            priority
-          />
-          <div className="flex gap-1">
-            <div>
-              <div className="text-2xl font-bold">Michael Slevin</div>
-              <div className="text-sm pb-2">
-                <div>Software Engineer and Car Enthusiast</div>
-                <div>B.S. Computer Science, Cal Poly, San Luis Obispo</div>
-                <div>Berkeley, CA</div>
-                <div>{address}</div>
-              </div>
-              <div className="flex flex-col">
-                <a
-                  href="https://www.linkedin.com/in/michaelslevin11/"
-                  className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
-                >
-                  LinkedIn
+    <main className="font-[Times_New_Roman] p-8 gap-8 flex">
+      <div className="flex flex-col gap-4">
+        <Image
+          src="/img/flowers_crop.jpeg"
+          alt="Me, standing in front of a flower bush."
+          width={200}
+          height={200}
+          priority
+        />
+        <div className="flex gap-1">
+          <div>
+            <div className="text-2xl font-bold">Michael Slevin</div>
+            <div className="text-sm pb-2">
+              <div>Software Engineer</div>
+              <div>Berkeley, CA</div>
+              <div>B.S. Computer Science</div>
+              <div>Cal Poly, San Luis Obispo</div>
+              <div>{address}</div>
+            </div>
+            <div className="flex flex-col">
+              {urls.map((url) => (
+                <a href={url.url} key={url.name}>
+                  {url.name}
                 </a>
-                <a
-                  href="https://github.com/mslevin"
-                  className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
-                >
-                  GitHub
-                </a>
-                <a
-                  href="https://www.instagram.com/michael_slevin/"
-                  className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
-                >
-                  Instagram
-                </a>
-                <a
-                  href="https://www.imnotspeeding.com"
-                  className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
-                >
-                  imnotspeeding.com
-                </a>
-              </div>
+              ))}
             </div>
           </div>
         </div>
-        <div>Content</div>
+      </div>
+      <div className="w-full">
+        <div className="flex flex-col gap-4 justify-center w-[90%] mx-auto">
+          <p>
+            I&apos;m Michael, an engineer at{" "}
+            <a target="_blank" href="https://www.complete.so">
+              Complete
+            </a>
+            . While my focus in the past has been primarily on the frontend,
+            I&apos;m always learning and I enjoy working across the stack. I
+            started at Complete in August 2023, and have worked on every aspect
+            of our stack and product offerings. Previously I worked at a startup
+            in Berkeley called{" "}
+            <a
+              target="_blank"
+              href="https://www.paymentsdive.com/news/grabango-ceases-operations/729589/"
+            >
+              Grabango
+            </a>
+            , where we were building computer-vision based retrofit solutions
+            for cashierless checkout- think Amazon Go, but better and less evil.
+          </p>
+          <p>
+            Growing up in Sunnyvale, I was surrounded by tech from a young age,
+            and have always enjoyed tinkering, modifying, breaking, and
+            sometimes fixing things. My first job was an internship where I
+            spent a summer doing IT Helpdesk Support. This was a challenging and
+            truly formative experience for me, and laid a foundation for the
+            customer-focused career I lead today. Empathy is a hugely underrated
+            skill in engineering.
+          </p>
+          <p>
+            When I&apos;m not at the computer I&apos;m usually driving, fixing,{" "}
+            <a target="_blank" href="https://youtu.be/iqFAuFdM_q0">
+              racing
+            </a>
+            , breaking, buying, or selling cars. I&apos;m a man of many hobbies,
+            but I&apos;ll always be a car guy first.
+          </p>
+          <Link href="/thoughts" className="text-lg ">
+            thoughts
+          </Link>
+        </div>
       </div>
     </main>
   );
